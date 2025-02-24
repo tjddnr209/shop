93b453e5f942c22995429353e4bca42ff1b3a116
wook <tjddnr209@naver.com>
2025-02-24 12:42:26 +0900

    Entity

---
+++ src/main/java/com/apple/shop/Item.java
@@ -0,0 +1,14 @@
+package com.apple.shop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String title;
+    public Integer price;
+}
