151e27a245eb90f345bffe0552200c58598c28fe
wook <tjddnr209@naver.com>
2025-02-24 11:50:15 +0900

    ItemController

---
+++ src/main/java/com/apple/shop/ItemController.java
@@ -0,0 +1,14 @@
+package com.apple.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+    @GetMapping("/list")
+    String list(Model model) {
+        model.addAttribute("name", "비싼 바지");
+        return "list.html";
+    }
+}
